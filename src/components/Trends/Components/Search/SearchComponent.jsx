d91ecf294f4960fb6bb96cf2b642c73fcc05131b
yassinediwani <yassinediwani444@gmail.com>
2023-05-20 22:56:59 +0100

    everything ready for you to start working on search

--- src/components/Trends/Components/Search/SearchComponent.jsx
+++ src/components/Trends/Components/Search/SearchComponent.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import SearchIcon from '../../../Icons/SearchIcon'
 import SettingsIcon from '../../../Icons/SettingsIcon'
 import './SearchComponent.css'
+import { useLocation } from 'react-router-dom'
 
 export default function SearchComponent({ hashtag }) {
+    const exlore = useLocation().pathname == 'explore';
+    console.log(exlore);
     return <div className="trends__search">
-        <div className="trend__header__search">
+        <div className={`trend__header__search ${exlore && 'padding'}`}>
         <div className="trend__search__input">
             <label className='search__icon center' htmlFor="search">
             <SearchIcon fill="#71767b" />
             </label>
             <input type="text" defaultValue={hashtag ? hashtag : ''} placeholder='Search Twitter' className='input trand__search__input' id='search' />
-        </div>
-        <div className="trend__search__parametre center">
-            <SettingsIcon />
         </div>
         </div>
     </div>  
