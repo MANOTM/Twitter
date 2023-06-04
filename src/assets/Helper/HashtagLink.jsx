e7871a8adb364604d5e083de15bdd9459af8a22e
yassinediwani <yassinediwani444@gmail.com>
2023-06-04 23:25:19 +0100

    now you can follow and like in bookmark and comment

--- src/assets/Helper/HashtagLink.jsx
+++ src/assets/Helper/HashtagLink.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const HashtagLink = ({ text }) => {
   const convertedText = text.replace(
     /#([\p{L}\p{N}_-]+)/gu,
@@ -5,3 +7,29 @@ export const HashtagLink = ({ text }) => {
   );
   return <span dangerouslySetInnerHTML={{ __html: convertedText }} />;
 };
+
+
+// import { Link } from 'react-router-dom';
+
+// export const HashtagLink = ({ text }) => {
+//   const convertedText = text.replace(
+//     /#([\p{L}\p{N}_-]+)/gu,
+//     (match, hashtag) => (
+//       <Link key={hashtag} className="underline" to={`/search/${hashtag}`}>
+//         #{hashtag}
+//       </Link>
+//     )
+//   );
+
+//   const renderConvertedText = () => {
+//     const parts = convertedText.split(/(#\p{L}\p{N}_-+)/gu);
+//     return parts.map((part, index) => {
+//       if (part.startsWith("#")) {
+//         return <React.Fragment key={index}>{React.cloneElement(part, { key: index })}</React.Fragment>;
+//       }
+//       return part;
+//     });
+//   };
+
+//   return <span>{renderConvertedText()}</span>;
+// };
--- src/pages/Comment/Comment.jsx
+++ src/pages/Comment/Comment.jsx
@@ -8,32 +8,37 @@ import { useNavigate, useParams } from 'react-router-dom'
 import useFetch from '../../hooks/useFetch'
 import Loading from '../../components/Loading/Loading'
 import CreateComment from './Component/CreateComment/CreateComment'
+import { NoLike } from '../../components/ProfileComponent/NoLike/NoLike'
+import { useStateContext } from '../../contexts/ContextProvider'
 
 export default function Comment() {
 
     // hello my friend if u find any issue try to fix it ,may get some hassanat
     const { idTweet } = useParams();
-    const navigate = useNavigate()
+    const { setHeadingCount } = useStateContext();
+    const navigate = useNavigate();
     const { loading, data } = useFetch('tweet/'+idTweet);
+    setHeadingCount('comments');
     return (
         <Main>
             <div className='reply__'>
                 <ProfileHead name='Tweet' />
                 {
                     loading ? <Loading /> : 
-                    data?.data?.tweet && 
+                    data?.data?.tweet ?
                     <>
-                        <Tweet tweet={data?.data?.tweet} />
+                        <Tweet Tweet4Comment={true} tweet={data?.data?.tweet} />
                         <CreateComment idTweet={idTweet} />
                         <div className="replys">
                             {
                                 loading ? <Loading /> :
                                 data?.data?.comments?.length ? 
-                                data?.data?.comments?.map((comment,i) => <CommentItem key={i} idTweet={idTweet} comment={comment} />) :
-                                null
+                                data?.data?.comments?.map((comment,i) => <CommentItem key={i} isComment={true} idTweet={idTweet} comment={comment} />) :
+                                <NoLike comment={true} any="comments" />
                             }
                         </div>
-                    </>
+                    </> :
+                    <NoLike TweetNotFound={true} any="comments" />
                 }
             </div>
         </Main>
